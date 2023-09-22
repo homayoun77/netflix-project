88d2224f9e801c3a57a7a6eaee9dad6d71fc2f11
Homayoun Norozy <homayounn77@gmail.com>
2023-09-22 18:46:20 +0330

    version 1.1.2 released

--- src/components/SavedShows.js
+++ src/components/SavedShows.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react'
 // import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 import { UserAuth } from '../context/AuthContext'
 import { db } from '../firebase'
-import { getDocs, collection, query, limit, startAfter, orderBy, deleteDoc, doc,  } from 'firebase/firestore'
+import { getDocs, collection, query, limit, startAfter, orderBy, deleteDoc, doc, } from 'firebase/firestore'
 
 import { AiOutlineClose } from 'react-icons/ai'
 
 function SavedShows() {
-  
+
   const { user } = UserAuth()
   const [movies, setMovies] = useState([])
 
@@ -21,10 +21,10 @@ function SavedShows() {
   //   slider.scrollLeft = slider.scrollLeft + 500
   // }
 
-  const [lastVisible , setLastVisible] = useState('')
+  const [lastVisible, setLastVisible] = useState('')
 
   const getData = async () => {
-    const first = query(collection(db, 'users', `${user?.email}`, 'favorite') , orderBy('title') , startAfter(lastVisible) , limit(10));
+    const first = query(collection(db, 'users', `${user?.email}`, 'favorite'), orderBy('title'), startAfter(lastVisible), limit(10));
     const data = await getDocs(first);
     setLastVisible(data.docs[data.docs.length - 1])
     data.forEach((doc) => {
@@ -41,10 +41,10 @@ function SavedShows() {
   const handelDelete = async (id) => {
     const result = movies.filter((item) => item.id !== id)
     setMovies(result)
-    await deleteDoc(doc(db , 'users' , `${user?.email}` , 'favorite' , `${id}`))
+    await deleteDoc(doc(db, 'users', `${user?.email}`, 'favorite', `${id}`))
   }
-  
-  
+
+
   // const movieRef = doc(db, 'users', `${user?.email}`)
   // const deleteShow = async (passedID) => {
   //   try {
@@ -68,20 +68,25 @@ function SavedShows() {
               <>
                 <div key={id} className='w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 inline-block relative cursor-pointer p-2'>
                   <img src={`https://image.tmdb.org/t/p/w500/${item?.img}`} alt={item?.title} />
-                  <div className='absolute top-0 left-0 w-full h-full opacity-0 hover:bg-black/60 hover:opacity-100 text-white'>
+                  <div className='hidden lg:block absolute top-0 left-0 w-full h-full opacity-0 hover:bg-black/60 hover:opacity-100 text-white'>
                     <p className='whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center text-center h-full'>{item?.title}</p>
                     <p onClick={() => handelDelete(item?.id)} className=' absolute top-4 right-4 text-gray-300 font-bold'><AiOutlineClose size={20} /></p>
                   </div>
+                  <div className='flex items-center justify-between lg:justify-center gap-x-10 mt-1 mx-1'>
+                    <p className='text-white text-center text-xs md:text-sm md:font-bold lg:text-base overflow-hidden'>
+                      {item?.title || item?.name}
+                    </p>
+                    <p className='lg:hidden text-gray-300' onClick={() => handelDelete(item?.id)}>
+                      <AiOutlineClose size={20} />
+                    </p>
+                  </div>
                 </div>
-                  {
-                  
-                  }
               </>
             ))
           }
         </div>
         <div>
-          <button className='text-gray-200 border border-gray-200 px-4 py-2 rounded my-8 font-bold hover:scale-110 transition-all' onClick={getData}>Show More</button>
+          <button className='text-gray-200 border border-gray-200 px-4 py-2 rounded my-8 font-bold md:active:scale-110 lg:hover:scale-110 transition-all' onClick={getData}>Show More</button>
         </div>
         {/* <MdChevronRight onClick={slideRight} size={30} className='bg-white absolute z-10 opacity-50 hover:opacity-100 cursor-pointer hidden group-hover:block mx-1 h-full right-0' /> */}
       </div>
